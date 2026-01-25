03e90424e747fd0a00e6d5c7a357c7489c23a1e5
Daniel <144605187+D4niel-Diaz@users.noreply.github.com>
2026-01-26 01:15:18 +0800

    fixed

--- config/cors.php
+++ config/cors.php
@@ -8,7 +8,7 @@ return [
 
     'allowed_origins' => [
         env('FRONTEND_URL', 'http://localhost:5173'),
-        env('FRONTEND_URL_PROD', 'http://localhost:5174'),
+        env('FRONTEND_URL_PROD', 'https://sochat-frontend.vercel.app'),
         'http://localhost:5175',
         'http://127.0.0.1:5175',
     ],
