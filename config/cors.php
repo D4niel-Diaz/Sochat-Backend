892061474757b84a9703500cbb0624e86a153e08
D4NIEL-DIAZ <danieldiesta00@gmail.com>
2026-01-26 16:34:26 +0800

    fix: Add port 5174 to CORS allowed origins

--- config/cors.php
+++ config/cors.php
@@ -22,6 +22,8 @@ return [
         'https://sochat-frontend-63y4.vercel.app',
         'http://localhost:5173',
         'http://127.0.0.1:5173',
+        'http://localhost:5174',
+        'http://127.0.0.1:5174',
     ],
 
     'allowed_origins_patterns' => [
